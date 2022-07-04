0e21f76e4eb285b30581730b85daf79109a67de2
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-07-04 14:56:57 -0700

    Add simulated TalonFX and PS4Controller
    Still need to add SingleJointedArmSim

--- MotionMagic/src/main/java/frc/robot/Robot.java
+++ MotionMagic/src/main/java/frc/robot/Robot.java
@@ -1,16 +1,52 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.simulation.PS4ControllerSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Robot extends TimedRobot {
+
+  PS4Controller ps4Controller = new PS4Controller(0);
+  PS4ControllerSim ps4ControllerSim = new PS4ControllerSim(ps4Controller);
+
+  WPI_TalonFX arm = new WPI_TalonFX(0);
+
+  // Create a SimCollection class for simulated inputs into the Talon
+  TalonFXSimCollection armSim = arm.getSimCollection();
+
+  @Override
+  public void robotInit() {
+    arm.configFactoryDefault();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    SmartDashboard.putNumber("Arm Position", arm.getSelectedSensorPosition());
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+    // Control the arm with percent output and ps4 controller
+    double movement = ps4Controller.getLeftY();
+    arm.set(ControlMode.PercentOutput, movement);
+    SmartDashboard.putNumber("PS4 Y-axis", movement);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+
+    // Pass battery voltage to simulated arm
+    armSim.setBusVoltage(RobotController.getBatteryVoltage());
+    SmartDashboard.putNumber("Arm Output Voltage", armSim.getMotorOutputLeadVoltage());
+
+    // TODO: Update sensors in SingleJointedArmSim here
+  }
+
 }
